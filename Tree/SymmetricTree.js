12b82100501a27807f57c59e5d102e8f5f06eeaf
邱立鋒 <lifo@qiulifengdeMac-mini.local>
2021-07-31 10:08:19 +0800

    trees problem

---
+++ Tree/SymmetricTree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  if (root === null) return true;
+
+  function helper(left, right) {
+    if (left === null && right === null) return true;
+    if (left === null || right === null) return false;
+    if (left.val !== right.val) return false;
+
+    return helper(left.left, right.right) && helper(left.right, right.left);
+  }
+
+  return helper(root.left, root.right);
+};
