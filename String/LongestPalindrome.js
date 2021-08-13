246629c2b5577264ab1e361b5525085c1df12d93
邱立鋒 <lifo@qiulifengdeMac-mini.local>
2021-08-13 22:02:09 +0800

    20210813

--- String/LongestPalindrome.js
+++ String/LongestPalindrome.js
@@ -1,1 +1,19 @@
-let longestPalindrome = function (s) {};
+let longestPalindrome = function (s) {
+  let map = new Map();
+  let result = 0;
+  for (i = 0; i < s.length; i++) {
+    map.set(s[i], map.get(s[i]) + 1 || 1);
+
+    if (map.get(s[i]) % 2 === 0) {
+      result += 2;
+    }
+  }
+  return s.length > result ? result + 1 : result;
+};
+
+/*
+Ex1 
+
+
+
+*/
