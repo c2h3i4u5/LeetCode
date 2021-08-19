ad838ce8d199b0189657a1c2e00b3722bd1293ac
邱立鋒 <lifo@qiulifengdeMac-mini.local>
2021-08-19 23:18:14 +0800

    20210819

--- Array/MaxConsecutiveOnes.js
+++ Array/MaxConsecutiveOnes.js
@@ -1,7 +1,3 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var findMaxConsecutiveOnes = function (nums) {
   let newArr = [];
   let max = 0;
@@ -34,3 +30,27 @@ findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
 //   }
 //   if (max_count < count) return count;
 //   return max_count;
+
+let findMaxConsecutiveOnes = function (nums) {
+  let count = 0;
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+    } else {
+      count = 0;
+    }
+    max = Math.max(max, count);
+  }
+  return max;
+};
+
+/*
+find the maximun number of consecutive of 1
+
+nums = [1,1,0,1,1,1] return 3
+
+when nums[i] = 1, count++
+when nums[i] = 0, compare max and count, and reset count
+
+*/
